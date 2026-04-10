5503806e40068c9c38c06bf574225654791a18e8
Emmanuel Otoo <eotoo218@gmail.com>
2026-04-10 17:11:56 +0000

    Refactor Layout component to include WarningBanner and FooterDisclaimer for enhanced user experience

--- src/layout.jsx
+++ src/layout.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/general/Navbar";
 import Footer from "./components/general/Footer";
+import WarningBanner from "./components/general/WarningBanner";
+import FooterDisclaimer from "./components/general/FooterDisclaimer";
 
 function Layout({ children }) {
 
   return (
     <>
+      <WarningBanner />
       <Navbar />
       <main>
         <Outlet /> {/* Child routes render here */}
       </main>
       <Footer />
+      <FooterDisclaimer />
     </>
   );
 }
