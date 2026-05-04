125936c4765af4ac7962eefb1d49cf9e8d4bb52b
Emmanuel Otoo <eotoo218@gmail.com>
2026-05-04 12:50:53 +0000

    Update API_BASE URL to use secure HTTPS for API requests

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://206.189.57.158:5000/api";
+const API_BASE = "https://api.emmanuelotoo.dev/api";
 
 async function request(endpoint, options = {}) {
   const res = await fetch(`${API_BASE}${endpoint}`, {
